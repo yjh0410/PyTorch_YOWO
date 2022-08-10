c608179abe507c5270eb8d6e887451c16532bb17
yjh0410 <1394571815@qq.com>
2022-08-10 11:10:06 +0800

    debug eval bash file

--- eval_ucf.sh
+++ eval_ucf.sh
@@ -6,7 +6,7 @@ python eval.py \
         -size 224 \
         --gt_folder ./evaluator/groundtruths_ucf_jhmdb/groundtruths_ucf/ \
         --dt_folder ./results/ucf_detections/detections_1/ \
-        --save_path ./results/ \
+        --save_path ./eval_results/ \
         --weight ./weights/ucf24/yowo-d19/yowof-d19_epoch_3.pth \
         --cal_mAP \
-        # --redo \
+        --redo \
