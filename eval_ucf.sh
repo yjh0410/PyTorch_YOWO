b93a1f0ecc613b615e9dd8f2abe82bac500b8274
yjh0410 <1394571815@qq.com>
2022-08-11 21:05:55 +0800

    eval yowo on ucf

--- eval_ucf.sh
+++ eval_ucf.sh
@@ -5,8 +5,8 @@ python eval.py \
         -bs 8 \
         -size 224 \
         --gt_folder ./evaluator/groundtruths_ucf_jhmdb/groundtruths_ucf/ \
-        --dt_folder ./results/ucf_detections/yowo/detections_1/ \
+        --dt_folder ./results/ucf_detections/detections_4/ \
         --save_path ./evaluator/eval_results/ \
-        --weight ./weights/ucf24/yowo-d19/yowo_80.4.pth \
+        --weight ./weights/ucf24/yowo/yowo_epoch_4_93.2_95.0.pth \
         --cal_mAP \
         # --redo \
