969ebfa6f2b2a372fabcbd535edd28c591259503
yjh0410 <1394571815@qq.com>
2022-09-10 09:44:36 +0800

    reg-5

--- evaluator/cal_mAP.py
+++ evaluator/cal_mAP.py
@@ -915,7 +915,6 @@ def get_mAP(gtFolder, detFolder, threshold = 0.5, savePath = None, datatset = 'u
 
     # Arguments validation
     errors = []
-    exit()
     # Validate formats
     gtFormat = ValidateFormats(gtFormat, 'gtFormat', errors)
     detFormat = ValidateFormats(detFormat, '-detformat', errors)
@@ -925,6 +924,7 @@ def get_mAP(gtFolder, detFolder, threshold = 0.5, savePath = None, datatset = 'u
     detCoordType = ValidateCoordinatesTypes(detCoordinates, '-detCoordinates', errors)
     imgSize = (0, 0)
 
+    exit()
     # Create directory to save results
     shutil.rmtree(savePath, ignore_errors=True)  # Clear folder
     # if savePath is not None:
