06af14823698ba3d20c18db844c4bf345b75d419
yjh0410 <1394571815@qq.com>
2022-09-10 09:46:14 +0800

    reg-5

--- evaluator/cal_mAP.py
+++ evaluator/cal_mAP.py
@@ -924,9 +924,9 @@ def get_mAP(gtFolder, detFolder, threshold = 0.5, savePath = None, datatset = 'u
     detCoordType = ValidateCoordinatesTypes(detCoordinates, '-detCoordinates', errors)
     imgSize = (0, 0)
 
-    # Create directory to save results
-    shutil.rmtree(savePath, ignore_errors=True)  # Clear folder
-    exit()
+    # # Create directory to save results
+    # shutil.rmtree(savePath, ignore_errors=True)  # Clear folder
+    # exit()
     # if savePath is not None:
     #     os.makedirs(savePath)
     # Show plot during execution
