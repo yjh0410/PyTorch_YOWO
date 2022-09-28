189d4228c972c6b2dfc06fab79452434d9aaa51d
yjh0410 <1394571815@qq.com>
2022-09-28 14:59:01 +0800

    video map

--- config/dataset_config.py
+++ config/dataset_config.py
@@ -6,7 +6,7 @@ dataset_config = {
         # dataset
         'data_root': '/mnt/share/ssd2/dataset/STAD/ucf24',
         # 'data_root': 'D:/python_work/spatial-temporal_action_detection/dataset/ucf24',
-        'gt_folder': './evaluator/groundtruth_ucf_jhmdb/groundtruth_ucf/',
+        'gt_folder': './evaluator/groundtruths_ucf_jhmdb/groundtruth_ucf/',
         # input size
         'train_size': 224,
         'test_size': 224,
@@ -59,7 +59,7 @@ dataset_config = {
         # dataset
         'data_root': '/mnt/share/ssd2/dataset/STAD/jhmdb21',
         'data_root': 'D:/python_work/spatial-temporal_action_detection/dataset/jhmdb21',
-        'gt_folder': './evaluator/groundtruth_ucf_jhmdb/groundtruth_jhmdb/',
+        'gt_folder': './evaluator/groundtruths_ucf_jhmdb/groundtruth_jhmdb/',
         # input size
         'train_size': 224,
         'test_size': 224,
