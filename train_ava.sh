cde59f61ace3eced91af73b694163e846a21b3e1
yjh0410 <1394571815@qq.com>
2022-10-09 10:56:34 +0800

    release yowo-nano

--- train_ava.sh
+++ train_ava.sh
@@ -2,8 +2,8 @@
 python train.py \
         --cuda \
         -d ava_v2.2 \
-        -v yowo \
+        -v yowo-nano \
         --num_workers 4 \
         --eval_epoch 1 \
-        --eval \
+        # --eval \
         # --fp16 \
